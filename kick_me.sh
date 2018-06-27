2446e402d39e4d2619e966f2bb3f993390648ac1
nesousx <denis.gourmel@gmail.com>
2018-06-27 11:46:50 +0200

    added files

---
+++ kick_me.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Dirty and stypid script to reinstall my computers...
+# 
+# Feel free to use, modifity, criticize, fork, etc.
+# Github: https://github.com/Nesousx/curious-couscous
+# 
+
+
+# Works under : Fedora !
+# Usage : curl -s https://raw.githubusercontent.com/nesousx/curious-couscous/kick_me.sh | /bin/bash
+
+sudo dnf install git ansible -y
+mkdir -p ~/Apps
+git clone https://github.com/Nesousx/curious-couscous.git ~/Apps/curious-couscous
+cd ~/Apps
+ansible-playbook fedora.yml
