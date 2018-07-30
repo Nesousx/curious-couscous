8eb573e9edf8788dc5d28398a5d41a685e17e18f
Denis <965185+Nesousx@users.noreply.github.com>
2018-07-30 09:00:55 +0200

    Update kick_me_ubuntu.sh

--- kick_me_ubuntu.sh
+++ kick_me_ubuntu.sh
@@ -10,7 +10,7 @@
 # Works under : Ubuntu !
 # Usage : sh -c "$(curl -fsSL https://raw.githubusercontent.com/Nesousx/curious-couscous/master/kick_me.sh)"
 
-apps="git libxcb-xrm-dev zsh tmux openssh vim firefox ranger libreoffice rofi xfce4-screenshooter rxvt-unicode xautolock keepassxc redshift numlockx xscreensaver ImageMagick nitrogen compton python-pip libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake"
+apps="git zsh tmux ssh vim firefox ranger libreoffice rofi xfce4-screenshooter rxvt-unicode xautolock redshift numlockx xscreensaver nitrogen compton python-pip libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf automake"
 bloats="transmission claws geany parole"
 #extra_apps="i3-gaps polybar rcm termite nextcloud-client"
 
